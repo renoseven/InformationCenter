b4d312862db8f83677836e54be5c6864363809e3
RenoSeven <dev@renoseven.net>
2016-10-18 00:02:12 -0700

    updated ui

--- app/src/main/java/net/renoseven/informationcenter/activity/SettingActivity.java
+++ app/src/main/java/net/renoseven/informationcenter/activity/SettingActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Switch;
 
@@ -19,6 +20,12 @@ public class SettingActivity extends AppCompatActivity {
     private final String TAG;
     private TrayPreferences appPreferences;
 
+    private View smsForwardingSettings;
+    private Switch smsForwardingEnable;
+
+    private View mailForwardingSettings;
+    private Switch mailForwardingEnable;
+
     public SettingActivity() {
         TAG = this.getClass().getSimpleName();
     }
@@ -32,34 +39,75 @@ public class SettingActivity extends AppCompatActivity {
         appPreferences = new ApplicationPreferences(this);
 
         Set<View> viewSet = findViews((ViewGroup) findViewById(R.id.scrollView));
-        for(View view : viewSet) {
+        for (View view : viewSet) {
             String viewTag = (String) view.getTag();
             if (view instanceof EditText) {
+                Log.d(TAG, viewTag + "=" + appPreferences.getString(viewTag, null));
                 ((EditText) view).setText(appPreferences.getString(viewTag, null));
-            }
-            else if (view instanceof Switch) {
+            } else if (view instanceof Switch) {
+                Log.d(TAG, viewTag + "=" + appPreferences.getBoolean(viewTag, false));
                 ((Switch) view).setChecked(appPreferences.getBoolean(viewTag, false));
             }
         }
         Log.i(TAG, "Settings loaded");
+
+        initComponents();
+    }
+
+    private void initComponents() {
+        // sms forwarding settings
+        smsForwardingSettings = findViewById(R.id.layout_sms_forwarding_setting);
+        smsForwardingEnable = (Switch) findViewById(R.id.sw_sms_forwarding_enable);
+        smsForwardingEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateUI();
+            }
+        });
+
+        // mail forwarding settings
+        mailForwardingSettings = findViewById(R.id.layout_mail_forwarding_setting);
+        mailForwardingEnable = (Switch) findViewById(R.id.sw_mail_forwarding_enable);
+        mailForwardingEnable.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                updateUI();
+            }
+        });
+
+        updateUI();
+    }
+
+    private void updateUI() {
+        // sms forwarding settings
+        if (!smsForwardingEnable.isChecked()) {
+            smsForwardingSettings.setVisibility(View.GONE);
+        } else {
+            smsForwardingSettings.setVisibility(View.VISIBLE);
+        }
+        // mail forwarding settings
+        if (!mailForwardingEnable.isChecked()) {
+            mailForwardingSettings.setVisibility(View.GONE);
+        } else {
+            mailForwardingSettings.setVisibility(View.VISIBLE);
+        }
     }
 
     @Override
     protected void onDestroy() {
         Log.d(TAG, "Saving changes...");
-//        SharedPreferences.Editor appEditor = applicationPref.edit();
 
         Set<View> viewSet = findViews((ViewGroup) findViewById(R.id.scrollView));
-        for(View view : viewSet) {
+        for (View view : viewSet) {
             String viewTag = (String) view.getTag();
             if (view instanceof EditText) {
                 appPreferences.put(viewTag, ((EditText) view).getText().toString());
-            }
-            else if (view instanceof Switch) {
+                Log.d(TAG, viewTag + "=" + appPreferences.getString(viewTag, null));
+            } else if (view instanceof Switch) {
                 appPreferences.put(viewTag, ((Switch) view).isChecked());
+                Log.d(TAG, viewTag + "=" + appPreferences.getBoolean(viewTag, false));
             }
         }
-//        appEditor.apply();
         Log.i(TAG, "Changes saved");
 
         super.onDestroy();
@@ -67,12 +115,11 @@ public class SettingActivity extends AppCompatActivity {
 
     private Set<View> findViews(ViewGroup g) {
         Set<View> set = new HashSet<>();
-        for(int i = 0; i < g.getChildCount(); i ++) {
+        for (int i = 0; i < g.getChildCount(); i++) {
             View view = g.getChildAt(i);
             if (view instanceof ViewGroup) {
                 set.addAll(findViews((ViewGroup) view));
-            }
-            else {
+            } else {
                 set.add(view);
             }
         }
--- app/src/main/java/net/renoseven/informationcenter/preference/ApplicationPreferences.java
+++ app/src/main/java/net/renoseven/informationcenter/preference/ApplicationPreferences.java
@@ -13,20 +13,22 @@ public class ApplicationPreferences extends TrayPreferences {
     private final static int DB_VERSION = 1;
 
     // Receiver settings
-    public final static String CONFIG_RECEIVER_SMS_ENABLED          = "receiver.sms.enabled";
-    public final static String CONFIG_RECEIVER_MAIL_ENABLED         = "mail.receiver.enabled";
+    public final static String CONFIG_RECEIVER_SMS_ENABLED          = "receiver.sms.enable";
+    public final static String CONFIG_RECEIVER_MAIL_ENABLED         = "receiver.mail.enable";
 
     // Mail settings
-    public final static String CONFIG_MAIL_SMTP_HOST                = "mail.smtp.host";
-    public final static String CONFIG_MAIL_SMTP_PORT                = "mail.smtp.port";
     public final static String CONFIG_MAIL_AUTH_USERNAME            = "mail.auth.username";
     public final static String CONFIG_MAIL_AUTH_PASSWORD            = "mail.auth.password";
+    public final static String CONFIG_MAIL_SMTP_HOST                = "mail.smtp.host";
+    public final static String CONFIG_MAIL_SMTP_PORT                = "mail.smtp.port";
+    public final static String CONFIG_MAIL_SMTP_SSL_ENABLED         = "mail.smtp.ssl.enable";
+    public final static String CONFIG_MAIL_SMTP_TLS_ENABLED         = "mail.smtp.tls.enable";
 
     // Forwarding settings
-    public final static String CONFIG_FORWARDING_SMS_ENABLED        = "forwarding.sms.enabled";
+    public final static String CONFIG_FORWARDING_SMS_ENABLED        = "forwarding.sms.enable";
     public final static String CONFIG_FORWARDING_SMS_RECEIVER       = "forwarding.sms.receiver";
 
-    public final static String CONFIG_FORWARDING_MAIL_ENABLED       = "forwarding.mail.enabled";
+    public final static String CONFIG_FORWARDING_MAIL_ENABLED       = "forwarding.mail.enable";
     public final static String CONFIG_FORWARDING_MAIL_SENDER        = "forwarding.mail.sender";
     public final static String CONFIG_FORWARDING_MAIL_SENDER_NAME   = "forwarding.mail.sender.name";
     public final static String CONFIG_FORWARDING_MAIL_RECEIVER      = "forwarding.mail.receiver";
